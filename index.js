6bb11db5ae127683cda22770f7e9713e97a4512c
Beta Priyoko with Sir Al Asror <beta.priyoko@students.amikom.ac.id>
2024-06-24 11:13:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce(function (acc, key, index) {
+    keys.reduce((acc, key, index) => {
       if (!acc || acc[key] === undefined) {
-        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+        throw new Error(`Path '${keys.slice(0, index + 1).join('.')}' does not exist.`);
       }
 
       if (index === keys.length - 1) {
@@ -60,13 +60,13 @@ class ImmutableUpdater {
     return new ImmutableUpdater(newObject);
   }
 
-  get(path = null) {
-    if (path && (typeof path !== 'string' || path === '')) {
+  get(path = '') {
+    if (typeof path !== 'string' || path === '') {
       throw new Error('Path must be a non-empty string.');
     }
 
-    const keys = ImmutableUpdater.parsePath(path || '');
-    return keys.reduce(function (acc, key) {
+    const keys = ImmutableUpdater.parsePath(path);
+    return keys.reduce((acc, key) => {
       return acc && acc[key] !== undefined ? acc[key] : undefined;
     }, this.obj);
   }
@@ -79,9 +79,9 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce(function (acc, key, index) {
+    keys.reduce((acc, key, index) => {
       if (!acc || acc[key] === undefined) {
-        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+        throw new Error(`Path '${keys.slice(0, index + 1).join('.')}' does not exist.`);
       }
 
       if (index === keys.length - 1) {
@@ -110,16 +110,16 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce(function (acc, key, index) {
+    keys.reduce((acc, key, index) => {
       if (!acc || acc[key] === undefined) {
-        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+        throw new Error(`Path '${keys.slice(0, index + 1).join('.')}' does not exist.`);
       }
 
       if (index === keys.length - 1) {
         if (!ImmutableUpdater.isObject(acc[key])) {
           acc[key] = {};
         }
-        acc[key] = Object.assign({}, acc[key], value);
+        acc[key] = { ...acc[key], ...value }; // Using spread syntax for merging
       } else {
         if (!ImmutableUpdater.isObject(acc[key])) {
           acc[key] = isNaN(keys[index + 1]) ? {} : [];
