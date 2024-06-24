a8de2f888f0807fb706ddbe115d324bca0a07323
Beta Priyoko with Sir Al Asror <beta.priyoko@students.amikom.ac.id>
2024-06-24 11:08:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,19 @@ class ImmutableUpdater {
     }
 
     const clone = Array.isArray(value) ? [] : {};
-    Object.entries(value).forEach(([key, val]) => {
-      clone[key] = ImmutableUpdater.cloneDeep(val);
-    });
+    for (let key in value) {
+      if (value.hasOwnProperty(key)) {
+        clone[key] = ImmutableUpdater.cloneDeep(value[key]);
+      }
+    }
     return clone;
   }
 
   static parsePath(path) {
+    if (typeof path !== 'string' || path === '') {
+      throw new Error('Path must be a non-empty string.');
+    }
+
     const regex = /[^.[\]]+/g;
     return path.match(regex) || [];
   }
@@ -32,10 +38,14 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce((acc, key, index) => {
+    keys.reduce(function (acc, key, index) {
+      if (!acc || acc[key] === undefined) {
+        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+      }
+
       if (index === keys.length - 1) {
         if (acc[key] === value) {
-          return acc[key]; // Skip if the value is the same
+          return acc[key];
         }
         acc[key] = value;
       } else {
@@ -51,12 +61,14 @@ class ImmutableUpdater {
   }
 
   get(path = null) {
-    if (!path) {
-      return this.obj;
+    if (path && (typeof path !== 'string' || path === '')) {
+      throw new Error('Path must be a non-empty string.');
     }
 
-    const keys = ImmutableUpdater.parsePath(path);
-    return keys.reduce((acc, key) => (acc && acc[key] !== undefined ? acc[key] : undefined), this.obj);
+    const keys = ImmutableUpdater.parsePath(path || '');
+    return keys.reduce(function (acc, key) {
+      return acc && acc[key] !== undefined ? acc[key] : undefined;
+    }, this.obj);
   }
 
   delete(path) {
@@ -67,12 +79,16 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce((acc, key, index) => {
+    keys.reduce(function (acc, key, index) {
+      if (!acc || acc[key] === undefined) {
+        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+      }
+
       if (index === keys.length - 1) {
         delete acc[key];
       } else {
         if (!ImmutableUpdater.isObject(acc[key])) {
-          return {}; // Early exit if the path does not exist
+          return {};
         }
         acc[key] = ImmutableUpdater.cloneDeep(acc[key]);
       }
@@ -94,12 +110,16 @@ class ImmutableUpdater {
     const keys = ImmutableUpdater.parsePath(path);
     const newObject = ImmutableUpdater.cloneDeep(this.obj);
 
-    keys.reduce((acc, key, index) => {
+    keys.reduce(function (acc, key, index) {
+      if (!acc || acc[key] === undefined) {
+        throw new Error('Path \'' + keys.slice(0, index + 1).join('.') + '\' does not exist.');
+      }
+
       if (index === keys.length - 1) {
         if (!ImmutableUpdater.isObject(acc[key])) {
           acc[key] = {};
         }
-        acc[key] = { ...acc[key], ...value };
+        acc[key] = Object.assign({}, acc[key], value);
       } else {
         if (!ImmutableUpdater.isObject(acc[key])) {
           acc[key] = isNaN(keys[index + 1]) ? {} : [];
